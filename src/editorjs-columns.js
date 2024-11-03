a3f6e86079e4e41c19747e66bbfb71ec4e1a8e0f
jasonhoendervanger <hoendervanger@eur.nl>
2024-11-03 11:07:45 +0100

    columns widen and npm update

--- src/editorjs-columns.js
+++ src/editorjs-columns.js
@@ -1,34 +1,26 @@
-/**
- * Column Block for the Editor.js.
- *
- * @author Calum Knott (calum@calumk.com)
- * @copyright Calum Knott
- * @license The MIT License (MIT)
- */
-
-/**
- * @typedef {Object} EditorJsColumnsData
- * @description Tool's input and output data format
- */
-
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import Swal from "sweetalert2";
-
 import icon from "./editorjs-columns.svg";
-
-// import EditorJS from '@editorjs/editorjs'; // required for npm mode
 
 const MAX_SPAN = 12;
 
 class EditorJsColumns {
-
 	static get enableLineBreaks() {
 		return true;
 	}
 
+	static get isReadOnlySupported() {
+		return true;
+	}
 
-	constructor({data, config, api, readOnly}) {
-		// start by setting up the required parts
+	static get toolbox() {
+		return {
+			icon: icon,
+			title: "Columns",
+		};
+	}
+
+	constructor({ data, config, api, readOnly }) {
 		this.api = api;
 		this.readOnly = readOnly;
 		this.config = config || {};
@@ -43,38 +35,20 @@ class EditorJsColumns {
 		}
 
 		this._data = {};
-
-		this.editors = {};
+		this.editors = {
+			cols: [],
+			numberOfColumns: Array.isArray(data.cols) ? data.cols.length : 2
+		};
 
 		this.colWrapper = undefined;
-
-		this.editors.cols = [];
-
 		this.data = data;
 
 		if (!Array.isArray(this.data.cols)) {
 			this.data.cols = [];
-			this.editors.numberOfColumns = 2;
-		} else {
-			this.editors.numberOfColumns = this.data.cols.length;
-
-
 		}
-		var n = this.data.cols.length || 2;
-		this.editors.spanOfColumns = [...Array(n).keys()].map(x => MAX_SPAN / n);
-	}
 
-	static get isReadOnlySupported() {
-		return true;
-	}
-
-
-	onKeyUp(e) {
-		// console.log(e)
-		// console.log("heyup")
-		if (e.code !== "Backspace" && e.code !== "Delete") {
-			return;
-		}
+		const n = this.data.cols.length || 2;
+		this.editors.spanOfColumns = [...Array(n).keys()].map(() => MAX_SPAN / n);
 	}
 
 	get CSS() {
@@ -84,254 +58,161 @@ class EditorJsColumns {
 		};
 	}
 
+	onKeyUp(e) {
+		if (e.code !== "Backspace" && e.code !== "Delete") {
+			return;
+		}
+	}
+
 	renderSettings() {
 		return [
 			{
 				icon: "2",
 				label: "2 Columns",
-				onActivate: () => {
-					this._updateCols(2)
-				}
+				onActivate: () => this._updateCols(2)
 			},
 			{
 				icon: "3",
 				label: "3 Columns",
-				onActivate: () => {
-					this._updateCols(3)
-				}
+				onActivate: () => this._updateCols(3)
 			},
 			{
 				icon: "R",
-				label: "Roll Colls",
-				onActivate: () => {
-					this._rollCols()
-				}
+				label: "Roll Cols",
+				onActivate: () => this._rollCols()
 			},
 			{
 				name: "Widen Cols",
 				icon: `<div>W</div>`,
-				onActivate: () => {
-					this._widenCols()
-				}
+				onActivate: () => this._widenCols()
 			}
-		]
+		];
 	}
 
-
 	_rollCols() {
-		// this shifts or "rolls" the columns
 		this.data.cols.unshift(this.data.cols.pop());
 		this.editors.cols.unshift(this.editors.cols.pop());
 		this._rerender();
 	}
 
 	async _updateCols(num) {
-		// Should probably update to make number dynamic... but this will do for now
-		if (num == 2) {
-			if (this.editors.numberOfColumns == 3) {
-				let resp = await Swal.fire({
-					title: "Are you sure?",
-					text: "This will delete Column 3!",
-					icon: "warning",
-					showCancelButton: true,
-					confirmButtonColor: "#3085d6",
-					cancelButtonColor: "#d33",
-					confirmButtonText: "Yes, delete it!",
-				});
+		if (num === 2 && this.editors.numberOfColumns === 3) {
+			const response = await Swal.fire({
+				title: "Are you sure?",
+				text: "This will delete Column 3!",
+				icon: "warning",
+				showCancelButton: true,
+				confirmButtonColor: "#3085d6",
+				cancelButtonColor: "#d33",
+				confirmButtonText: "Yes, delete it!",
+			});
 
-				if (resp.isConfirmed) {
-					this.editors.numberOfColumns = 2;
-					this.data.cols.pop();
-					this.editors.cols.pop();
-					this._rerender();
-				}
+			if (response.isConfirmed) {
+				this.editors.numberOfColumns = 2;
+				this.data.cols.pop();
+				this.editors.cols.pop();
+				this._rerender();
 			}
 		}
-		if (num == 3) {
+
+		if (num === 3) {
 			this.editors.numberOfColumns = 3;
 			this._rerender();
-			// console.log(3);
 		}
 	}
 
-
 	async _widenCols() {
-
-		if (this.editors.numberOfColumns == 2) {
-			var [s1, s2] = this.editors.spanOfColumns;
-			s1 += 1;
-			if (s1 > 11) s1 = 1;
+		if (this.editors.numberOfColumns === 2) {
+			let [s1, s2] = this.editors.spanOfColumns;
+			s1 = s1 + 1 > 11 ? 1 : s1 + 1;
 			s2 = MAX_SPAN - s1;
 			this.editors.spanOfColumns = [s1, s2];
-			this._rerender();
-
-		} else if (this.editors.numberOfColumns == 3) {
-			var [s1, s2, s3] = this.editors.spanOfColumns;
-			s1 += 1;
-			if (s1 > 10) s1 = 1;
-			s2 = parseInt((MAX_SPAN - s1) / 2);
-			s3 = MAX_SPAN - s1 - s2;
-
+		} else if (this.editors.numberOfColumns === 3) {
+			let [s1] = this.editors.spanOfColumns;
+			s1 = s1 + 1 > 10 ? 1 : s1 + 1;
+			const s2 = parseInt((MAX_SPAN - s1) / 2);
+			const s3 = MAX_SPAN - s1 - s2;
 			this.editors.spanOfColumns = [s1, s2, s3];
-
-			this._rerender();
-
 		}
+
+		this._rerender();
 	}
 
 	async _rerender() {
 		await this.save();
+		this._destroyEditors();
+		this._createColumns();
+	}
 
-
-		for (let index = 0; index < this.editors.cols.length; index++) {
-			this.editors.cols[index].destroy();
-		}
+	_destroyEditors() {
+		this.editors.cols.forEach(editor => editor.destroy());
 		this.editors.cols = [];
-
 		this.colWrapper.innerHTML = "";
+	}
 
-
+	_createColumns() {
 		for (let index = 0; index < this.editors.numberOfColumns; index++) {
-
-			let col = document.createElement("div");
-			col.classList.add("ce-editorjsColumns_col");
-			col.classList.add("editorjs_col_" + index);
-			col.classList.add(`ce-editorjsColumns_span-${this.editors.spanOfColumns[index]}`);
-
-
-			let editor_col_id = uuidv4();
-
-			col.id = editor_col_id;
-
+			const col = this._createColumn(index);
 			this.colWrapper.appendChild(col);
-
-			let editorjs_instance = new this.config.EditorJsLibrary({
-				defaultBlock: "paragraph",
-				holder: editor_col_id,
-				tools: this.config.tools,
-				data: this.data.cols[index],
-				readOnly: this.readOnly,
-				minHeight: 50,
-			});
-
-			this.editors.cols.push(editorjs_instance);
+			this.editors.cols.push(this._createEditor(col.id, index));
 		}
 	}
 
+	_createColumn(index) {
+		const col = document.createElement("div");
+		col.classList.add(
+			"ce-editorjsColumns_col",
+			`editorjs_col_${index}`,
+			`ce-editorjsColumns_span-${this.editors.spanOfColumns[index]}`
+		);
+		col.id = uuidv4();
+		return col;
+	}
+
+	_createEditor(holderId, index) {
+		return new this.config.EditorJsLibrary({
+			defaultBlock: "paragraph",
+			holder: holderId,
+			tools: this.config.tools,
+			data: this.data.cols[index],
+			readOnly: this.readOnly,
+			minHeight: 50,
+		});
+	}
+
 	render() {
-
-		// This is needed to prevent the enter / tab keys - it globally removes them!!!
-
-
-		// // it runs MULTIPLE times. - this is not good, but works for now
-
-
-		// console.log("Generating Wrapper");
-
-		// console.log(this.api.blocks.getCurrentBlockIndex());
-
 		this.colWrapper = document.createElement("div");
 		this.colWrapper.classList.add("ce-editorjsColumns_wrapper");
 
+		this._setupEventListeners();
+		this._createColumns();
 
-		// astops the double paste issue
+		return this.colWrapper;
+	}
+
+	_setupEventListeners() {
 		this.colWrapper.addEventListener('paste', (event) => {
-			// event.preventDefault();
 			event.stopPropagation();
 		}, true);
 
-
 		this.colWrapper.addEventListener('keydown', (event) => {
-
-			// if (event.key === "Enter" && event.altKey) {
-			// 	console.log("ENTER ALT Captured")
-			// 	console.log(event.target)
-
-			// 	// let b = event.target.dispatchEvent(new KeyboardEvent('keyup',{'key':'a'}));
-
-			// 	event.target.innerText += "Aß"
-
-			// 	// console.log(b)
-			// }
-			// else 
-			if (event.key === "Enter") {
+			if (event.key === "Enter" || event.key === "Tab") {
 				event.preventDefault();
 				event.stopImmediatePropagation();
 				event.stopPropagation();
-
-				// console.log("ENTER Captured")
-				// this.api.blocks.insertNewBlock({type : "alert"});
-				// console.log("Added Block")
-			}
-			if (event.key === "Tab") {
-				// event.stopImmediatePropagation();
-				event.preventDefault();
-				event.stopImmediatePropagation();
-				event.stopPropagation();
-
-				// console.log("TAB Captured")
 			}
 		});
-
-		// console.log("Generating Wrapper");
-		for (let index = 0; index < this.editors.cols.length; index++) {
-			this.editors.cols[index].destroy();
-		}
-
-		// console.log(this.editors.cols);
-		this.editors.cols = []; //empty the array of editors
-		// console.log(this.editors.cols);
-
-		// console.log("Building the columns");
-
-		for (let index = 0; index < this.editors.numberOfColumns; index++) {
-			// console.log("Start column, ", index);
-			let col = document.createElement("div");
-			col.classList.add("ce-editorjsColumns_col");
-			col.classList.add("editorjs_col_" + index);
-			col.classList.add(`ce-editorjsColumns_span-${this.editors.spanOfColumns[index]}`);
-
-			let editor_col_id = uuidv4();
-			// console.log("generating: ", editor_col_id);
-			col.id = editor_col_id;
-
-			this.colWrapper.appendChild(col);
-
-			let editorjs_instance = new this.config.EditorJsLibrary({
-				defaultBlock: "paragraph",
-				holder: editor_col_id,
-				tools: this.config.tools,
-				data: this.data.cols[index],
-				readOnly: this.readOnly,
-				minHeight: 50,
-			});
-
-			this.editors.cols.push(editorjs_instance);
-			var n = this.data.cols.length || 2;
-			this.editors.spanOfColumns = [...Array(n).keys()].map(x => MAX_SPAN / n);
-			// console.log("End column, ", index);
-		}
-		return this.colWrapper;
 	}
 
 	async save() {
 		if (!this.readOnly) {
-			// console.log("Saving");
 			for (let index = 0; index < this.editors.cols.length; index++) {
-				let colData = await this.editors.cols[index].save();
-				this.data.cols[index] = colData;
+				this.data.cols[index] = await this.editors.cols[index].save();
 			}
 		}
-		this.data.spans = this.editors.spanOfColumns
+		this.data.spans = this.editors.spanOfColumns;
 		return this.data;
-	}
-
-	static get toolbox() {
-		return {
-			icon: icon,
-			title: "Columns",
-		};
 	}
 }
 
-export {EditorJsColumns as default};
+export default EditorJsColumns;
